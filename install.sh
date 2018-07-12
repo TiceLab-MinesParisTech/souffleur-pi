d4dfec314503e7b2d53a931b3da73fb71d0bda20
nico <nico@picapo.net>
2018-07-12 23:35:18 +0200

    * début d'un script d'installation

---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#Check privilege
+if [ "$EUID" -ne 0 ]; then
+	echo "Please run as root"
+	exit
+fi
+
+#Global vars
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+CONF_PATH=$SCRIPT_PATH/conf
+
+function copy_conf_file {
+	SRC=$CONF_PATH$1
+	DST=$1
+	echo "Copy $DST"
+}
+
+#Installation
+echo "* Install dependencies"
+apt-get install openssh-server bridge-utils hostapd dnsmasq avahi-daemon
+
+echo "* Activate ssh daemon"
+systemctl enable ssh
+
+echo "* Configure network"
+copy_conf_file "/etc/network/interfaces"
+service networking restart
+
+echo "* Configure Hostapd"
+copy_conf_file /etc/hostapd/hostapd.conf
+copy_conf_file /etc/default/hostapd
+#systemctl unmask hostapd.service
+systemctl enable hostapd.service
+
+echo "* Configure Dnsmasq"
+copy_conf_file /etc/dnsmasq.conf
+copy_conf_file /etc/hosts
+systemctl enable hostapd.service
+
+echo "* Configure Avahi-daemon"
+copy_conf_file /etc/hostname
+copy_conf_file /etc/avahi/avahi-daemon.conf
+systemctl enable avahi-daemon
+
+
